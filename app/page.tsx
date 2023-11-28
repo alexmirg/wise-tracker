04163f338b1b678923737e439643190c4185e1da
Oleksii Rohochyi <o.rohochyi@gmail.com>
2023-11-28 01:18:07 +0100

    Implemented hero carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 
@@ -25,7 +26,7 @@ const Home = () => {
 
             <Searchbar />
           </div>
-          HeroCarousel
+          <HeroCarousel />
         </div>
       </section>
       <section className="trending-section">
---
+++ components/HeroCarousel.tsx
@@ -0,0 +1,57 @@
+"use client"
+import React from 'react'
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+import Image from 'next/image'
+
+const heroImages = [
+ {
+    imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'
+ },
+ {
+    imgUrl: '/assets/images/hero-2.svg', alt: 'bag'
+ },
+ {
+    imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'
+ },
+ {
+    imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'
+ },
+ {
+    imgUrl: '/assets/images/hero-5.svg', alt: 'chair'
+ },
+]
+
+function HeroCarousel() {
+  return (
+    <div className='hero-carousel'>
+        <Carousel
+            showThumbs={false}
+            autoPlay
+            infiniteLoop
+            interval={2000}
+            showArrows={false}
+            showStatus={false}>
+            {heroImages.map((image) => (
+                <Image
+                    key={image.alt}
+                    src={image.imgUrl}
+                    alt={image.alt}
+                    width={484}
+                    height={484}
+                    className='object-contain'
+                />
+            ))}
+        </Carousel>
+        <Image 
+            className='max-xl:hidden absolute -left-[15%] bottom-0'
+            src="/assets/icons/hand-drawn-arrow.svg"
+            alt="arrow"
+            width={175}
+            height={175} />
+    </div>
+  )
+}
+
+export default HeroCarousel
