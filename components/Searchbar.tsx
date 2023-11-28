539b02873a586f65958361b1360dd872e6e47c9d
Oleksii Rohochyi <o.rohochyi@gmail.com>
2023-11-28 01:36:44 +0100

    Prepared Searchbar component for scratching

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,16 +1,61 @@
 "use client"
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (url: string): Boolean => {
+    try {
+        const parsedURL = new URL(url);
+        const hostname = parsedURL.hostname;
+
+        if(
+            hostname.includes('amazon.com') ||
+            hostname.includes('amazon.') ||
+            hostname.endsWith('amazon')) {
+                return true;
+            }
+    } catch (err) {
+        return false;
+    }
+
+    return false;
+}
 
 function Searchbar() {
-    const handleSubmit = () => {}
+    const [searchPrompt, setSearchPrompt] = useState<string>('')
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+        if (!isValidLink) {
+            return alert('Please provide a valid Amazon link');
+        }
+
+        try { 
+            setIsLoading(true)
+            // scrape the product page
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
     return (
         <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
             <input 
                 type="text"
                 placeholder='Enter product link'
-                className='searchbar-input' />
-            <button type='submit' className='searchbar-btn'>Search</button>
+                className='searchbar-input' 
+                value={searchPrompt}
+                onChange={(e) => setSearchPrompt(e.target.value)}/>
+            <button
+                type='submit'
+                disabled={searchPrompt.length <= 3}
+                className='searchbar-btn'>
+                    {isLoading ? 'Searching...' : 'Search'}
+                </button>
         </form>
     )
 }
