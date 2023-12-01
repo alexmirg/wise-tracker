7e8feb2acb2b1fd00dc259de04ad9ffc481d71ad
Oleksii Rohochyi <o.rohochyi@gmail.com>
2023-12-01 01:26:15 +0100

    Fixed deployment issues v1

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300
+export const maxDuration = 5
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
