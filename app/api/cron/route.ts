bb333eeae4d4491108200a825e773aef9a4a064c
Oleksii Rohochyi <o.rohochyi@gmail.com>
2023-12-01 01:21:41 +0100

    Fixed deployment issues

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET() {
 try {
     connectToDB()
@@ -39,7 +43,7 @@ try {
             }
 
             const updatedProduct = await Product.findOneAndUpdate(
-                {url: scrapedProduct.url},
+                {url: product.url},
                 product
             )
             
